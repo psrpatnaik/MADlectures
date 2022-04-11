1a528041c04fbf8fd78dd6851ff9b7822906a5a1
psrpatnaik <psrpatnaik@gmail.com>
2022-04-11 16:03:19 +0530

    Shared Preferences

--- app/src/main/java/com/example/button_tap_2/MainActivity2.kt
+++ app/src/main/java/com/example/button_tap_2/MainActivity2.kt
@@ -13,8 +13,10 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
         var b = findViewById<Button>(R.id.button2);
         b.setOnClickListener(View.OnClickListener {
+        // Explicit Intent, you need to associate an activity using FQN see the below line.
        var intent:Intent = Intent(this, Class.forName("com.example.button_tap_2.MainActivity"))
-        startActivity(intent)
+        // startActivity is used to invoke other activity associated to an Intent object.
+            startActivity(intent)
         })
     }
 }
